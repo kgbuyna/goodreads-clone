a64300a1336f6e2185d7195e7d26a4cd83159527
kgbuyna <kgbuyna692@gmail.com>
2022-12-10 09:59:38 +0800

    homescreen

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/Screens/HomeScreen';
+import NavigationBar from './src/Components/NavigationBar';
+
 
 export default function App() {
+  
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* screenOptions руу хэвлэгдэх tab aa өгөх.  */}
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}  options={{headerShown: false}} />
+      </Stack.Navigator>
+      <NavigationBar/>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
